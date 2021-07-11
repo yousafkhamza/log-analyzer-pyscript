f505bb1345ebbd8df2b7caf360e4ccd90adc786c
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2021-07-11 07:42:17 +0530

    update commit

--- log.py
+++ log.py
@@ -6,7 +6,7 @@ import os
 def get_hit(t):
     return t[1]
 
-path = input('Enter your log file name (include path): ')
+path = input('Enter your log file name (absalute path): ')
 
 if path.lower().split('/')[-1].endswith('log') and os.path.isfile('{}'.format(path)):
     file = open("{}".format(path),'r')
@@ -22,7 +22,7 @@ if path.lower().split('/')[-1].endswith('log') and os.path.isfile('{}'.format(pa
     result = sorted(ipcount.items(),key=get_hit,reverse=True)[:10]
     for item in result:
         ip,hit = item
-        country = ipstack.get_country(ip=ip,key=api.api)
+        country = ipstack.get_country(ip=ip,key=apikey.api)
         print("{:20}:{:10}  [{}]".format(ip,hit,country))
 else:
     print('This is not a access_log')
